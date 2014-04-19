cd0c678061996a1f8d836f9c9775e4aa3630fa62
Nathan Rajlich <nathan@tootallnate.net>
2014-04-19 16:21:38 -0700

    lots more progress

--- index.js
+++ index.js
@@ -3,7 +3,16 @@
  * Module dependencies.
  */
 
+var uid = require('uid');
 var event = require('event');
+var Promise = require('promise');
+var debug = require('debug')('wpcom-cookie-auth');
+
+/**
+ * Export `request` function.
+ */
+
+module.exports = Promise.nodeify(request);
 
 /**
  * WordPress.com REST API base endpoint.
@@ -12,13 +21,38 @@ var event = require('event');
 var proxyOrigin = 'https://public-api.wordpress.com';
 
 /**
+ * "Origin" of the current HTML page.
+ */
+
+var origin = window.location.protocol + '//' + window.location.hostname;
+debug('using "origin": %s', origin);
+
+/**
  * Reference to the <iframe> DOM element.
  * Gets set in the install() function.
  */
 
 var iframe;
 
-module.exports = request;
+/**
+ * Set to `true` upon the iframe's "load" event.
+ */
+
+var loaded = false;
+
+/**
+ * Array of buffered API requests. Added to when API requests are done before the
+ * proxy <iframe> is "loaded", and fulfilled once the "load" DOM event on the
+ * iframe occurs.
+ */
+
+var buffered;
+
+/**
+ * In-flight API request Promise instances.
+ */
+
+var requests = {};
 
 /**
  * Performs a "proxied REST API request". This happens by calling
@@ -31,13 +65,42 @@ module.exports = request;
  * @api public
  */
 
-function request (params, fn) {
+function request (params) {
+  debug('request()', params);
+
   if ('string' == typeof params) {
     params = { path: params };
   }
+  debug('params object:', params);
 
   // inject the <iframe> upon the first proxied API request
   if (!iframe) install();
+
+  // generate a uid for this API request
+  var id = uid();
+
+  var req = new Promise(function (resolve, reject) {
+    if (loaded) {
+      debug('sending API request to proxy <iframe>');
+      submitRequest(params, resolve, reject);
+    } else {
+      debug('buffering API request since proxying <iframe> is not yet loaded');
+      buffered.push([params, resolve, reject]);
+    }
+  });
+
+  // store the Promise so that "onmessage" can access it again
+  requests[id] = req;
+
+  return req;
+}
+
+/**
+ *
+ */
+
+function submitRequest (params) {
+  iframe.contentWindow.postMessage(params, proxyOrigin);
 }
 
 /**
@@ -48,4 +111,50 @@ function request (params, fn) {
  */
 
 function install () {
+  debug('install()');
+  if (iframe) uninstall();
+
+  buffered = [];
+
+  // listen to messages sent to `window`
+  event.bind(window, 'message', onmessage);
+
+  // create the <iframe>
+  iframe = document.createElement('iframe');
+  iframe.src = proxyOrigin + '/rest-proxy/#' + origin;
+  iframe.style.display = 'none';
+
+  // set `loaded` to true once the "load" event happens
+  event.bind(iframe, 'load', onload);
+
+  // inject the <iframe> into the <body>
+  document.body.appendChild(iframe);
+}
+
+/**
+ * The proxy <iframe> instance's "load" event callback function.
+ *
+ * @param {Event} e
+ * @api private
+ */
+
+function onload (e) {
+  debug('proxy <iframe> "load" event');
+  loaded = true;
+
+  // flush any buffered API calls
+  for (var i = 0; i < buffered.length; i++) {
+    submitRequest.apply(null, buffered[i]);
+  }
+}
+
+/**
+ * The main `window` object's "message" event callback function.
+ *
+ * @param {Event} e
+ * @api private
+ */
+
+function onmessage (e) {
+  console.log('onmessage', e);
 }
